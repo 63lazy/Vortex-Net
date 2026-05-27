53f6f04abbc2ce79d914e8894f9d46f02b40a71a
63Lazy <1294123119@qq.com>
2026-05-27 19:29:32 +0800

    节点选择读写时使用写时复制替代加锁

--- Proxy/src/Selector.cc
+++ Proxy/src/Selector.cc
@@ -4,35 +4,45 @@
 std::atomic_int Selector::total_weight=0;
 std::atomic_int Selector::failed_requests=0;
 void Selector::addNode(const std::shared_ptr<ServerNode> node){
-    //待改进：写时复制而非加锁
-    {
-        std::unique_lock<std::mutex> lock(mutex_);
-        serverGroup_.emplace_back(node);
+    //修改节点必须加锁防止重入
+    std::unique_lock<std::mutex> lock(mutex_);
+    //说明此时有线程在读
+    if(!serverGroup_.unique()){
+        //写时复制逻辑
+        //写操作直接操作新副本
+        serverGroup_.reset(new ServerNodeList(*serverGroup_));
     }
+    serverGroup_->emplace_back(node);
 }   
 
 InetAddress Selector::getNextServer(){
-    //待改进：写时复制而非加锁
-    std::unique_lock<std::mutex> lock(mutex_);
+    //高频操作 如果用加锁的方式会导致性能下降
+    //采取写时复制
+    std::shared_ptr<ServerNodeList> nodes;
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        nodes=serverGroup_;
+    }
+
     //集群组中没有服务器
-    if(serverGroup_.empty()) 
-    {   
+    if((*nodes).empty()) 
+    {
         LOG_ERROR("No available backend servers!");
         failed_requests++;
         return InetAddress(0,"0.0.0.0");
     }
     //统计活着的节点比例
     int alive_count = 0;
-    for(const auto& node : serverGroup_) {
+    for(const auto& node : (*nodes)) {
         if(node->isAlive()) alive_count++;
     }
     //判定是否触发 Panic Mode
     //如果活着的节点太少，说明 HealthChecker 可能失真，强制“全量复活”
-    bool panic_mode = (alive_count * 100 / serverGroup_.size()) < 20;
+    bool panic_mode = (alive_count * 100 / (*nodes).size()) < 20;
 
     ServerNode* best = nullptr;
     int total = 0;
-    for (auto& node : serverGroup_) {
+    for (auto& node : (*nodes)) {
         if (!node->isAlive()&&!panic_mode) continue;
         
         node->current_weight_ += node->getWeight();
--- Proxy/src/Selector.h
+++ Proxy/src/Selector.h
@@ -9,11 +9,13 @@
 #include "ServerNode.h"
 class Selector{
 public:
+    using ServerNodeList=std::vector<std::shared_ptr<ServerNode>>;
     Selector(EventLoop *loop):loop_(loop){}
     void addNode(const std::shared_ptr<ServerNode> node);
     InetAddress getNextServer();
 private:
-    std::vector<std::shared_ptr<ServerNode>> serverGroup_;
+    //配合写时复制使用
+    std::shared_ptr<ServerNodeList> serverGroup_;
     EventLoop *loop_;
     
     std::mutex mutex_;
